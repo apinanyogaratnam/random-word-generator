d6092e9e914e47fd424a62dfa94573f19cbf7c5f
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-08-21 19:09:31 -0400

    removed unnecessary import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 function App() {
   const [data, setData] = useState([]);
