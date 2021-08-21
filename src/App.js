f972687a99443a7095663d07424e152b168d7afc
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-08-21 17:17:54 -0400

    added name

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 
 function App() {
   console.log("atlanta");
+  const name = "atlantas name";
 
   return (
     <div className="App">
       <h1>Hello There Mr.Atlanta</h1>
+      <p>Your name is {name}</p>
     </div>
   );
 }
